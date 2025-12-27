ce7dbe0b372eec69848502992b42751899ecbcc5
Alexander Hougaard <alexander@hougaard.org>
2025-12-27 19:15:10 +0100

    Add files via upload

---
+++ motor.h
@@ -0,0 +1,11 @@
+// Skrevet af Alexander, Oliver og Phillip
+
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+void moveForward(int speed);
+void moveBackward(int speed);
+void stopMotor(void);
+void Motor_Init(void);
+
+#endif
